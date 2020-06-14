65fee996705761ab2dbda6138e8441f1aa7d3d29
blashadow <blackzerogamer@gmail.com>
2020-06-14 15:58:38 -0400

    Constants files

---
+++ AudioFoundationTest/Domain/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  AudioFoundationTest
+//
+//  Created by Luis Romero on 6/14/20.
+//  Copyright © 2020 Luis Romero. All rights reserved.
+//
+
+import UIKit
+
+struct FontNames {
+  struct Montserrat {
+    static let bold = "Montserrat-Bold"
+    static let light = "Montserrat-Light"
+    static let medium = "Montserrat-Medium"
+    static let regular = "Montserrat-Regular"
+    static let semiBold = "Montserrat-SemiBold"
+    static let thin = "Montserrat-Thin"
+  }
+}
