75c653ba79d3b6536a1ee80074ab166b7413dc13
blashadow <blackzerogamer@gmail.com>
2020-06-16 18:51:33 -0400

    audio parsing extension

---
+++ AudioFoundationTest/Extensions/TimeIntervalHelper.swift
@@ -0,0 +1,21 @@
+//
+//  TimeIntervalHelper.swift
+//  AudioFoundationTest
+//
+//  Created by Luis Romero on 6/16/20.
+//  Copyright © 2020 Luis Romero. All rights reserved.
+//
+
+import UIKit
+
+class TimeIntervalHelper: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
