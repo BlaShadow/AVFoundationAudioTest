d57da288b6067bf7cb4a35c7eec42d801334430b
blashadow <blackzerogamer@gmail.com>
2020-06-16 09:28:05 -0400

    Recording domain class

---
+++ AudioFoundationTest/Domain/Recording.swift
@@ -0,0 +1,15 @@
+//
+//  Recording.swift
+//  AudioFoundationTest
+//
+//  Created by Luis Romero on 6/8/20.
+//  Copyright © 2020 Luis Romero. All rights reserved.
+//
+
+import UIKit
+
+struct Recording {
+  let name: String
+  let path: URL
+  let duration: Double
+}
