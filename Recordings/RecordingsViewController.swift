c54aea9c45c038ec4011fc0898963efcf3e8c4e5
blashadow <blackzerogamer@gmail.com>
2020-06-16 09:29:44 -0400

    recordings view controller

---
+++ Recordings/RecordingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RecordingsViewController.swift
+//  AudioFoundationTest
+//
+//  Created by Luis Romero on 6/14/20.
+//  Copyright © 2020 Luis Romero. All rights reserved.
+//
+
+import UIKit
+
+class RecordingsViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad() 
+  }
+}
