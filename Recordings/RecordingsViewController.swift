639ac5167d7b58815347dc4f689e74130265b9f2
blashadow <blackzerogamer@gmail.com>
2020-06-17 17:53:31 -0400

    Display list of recordings

--- Recordings/RecordingsViewController.swift
+++ Recordings/RecordingsViewController.swift
@@ -9,7 +9,47 @@
 import UIKit
 
 class RecordingsViewController: UIViewController {
+  var recordingsView: RecordingsCollectionView?
+  
+  let recordingsDataSource = RecordingsDataSource()
+  let layoutDelegate = RecordingsCollectionLayout()
+  
+  var recordings: [Recording] = []
+
   override func viewDidLoad() {
-    super.viewDidLoad() 
+    super.viewDidLoad()
+
+    self.recordingsView = RecordingsCollectionView()
+
+    if let view = self.recordingsView {
+      self.view.addSubview(view)
+      
+      NSLayoutConstraint.activate([
+        view.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+        view.heightAnchor.constraint(equalTo: self.view.heightAnchor),
+        view.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+        view.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+      ])
+      
+      view.collectionView.register(RecordingCollectionViewCell.self, forCellWithReuseIdentifier: "CELL")
+      view.collectionView.dataSource = self.recordingsDataSource
+      view.collectionView.delegate = self.layoutDelegate
+      view.collectionView.reloadData()
+    }
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    self.recordings = DataBaseAcess.savedRecordings().sorted(by: { (a, b) -> Bool in
+      return a.created > b.created
+    })
+
+    self.reloadData()
+  }
+  
+  private func reloadData() {
+    self.recordingsDataSource.items = self.recordings
+    self.recordingsView?.collectionView.reloadData()
   }
 }
