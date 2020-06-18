7bc2230bdc07500909dc8beb17ae30dba0c98d64
blashadow <blackzerogamer@gmail.com>
2020-06-18 07:17:43 -0400

    collection view click handler

--- AudioFoundationTest/RecordingsCollectionLayout.swift
+++ AudioFoundationTest/RecordingsCollectionLayout.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class RecordingsCollectionLayout: NSObject, UICollectionViewDelegateFlowLayout {
+  var selectionHandler: ((_ index: Int) -> Void)?
+
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    if let handler = self.selectionHandler {
+      handler(indexPath.row)
+    }
+  }
+
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
     return CGSize(width: collectionView.frame.width - 20, height: 75)
   }
